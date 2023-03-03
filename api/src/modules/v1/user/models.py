7206ab6e97c2b2a1750b018b53822097a60d7305
zardarian <zardarianahadika@gmail.com>
2023-03-03 19:05:36 +0700

    update field & queries

--- api/src/modules/v1/user/models.py
+++ api/src/modules/v1/user/models.py
@@ -8,13 +8,13 @@ class User(models.Model):
     updated_by = models.CharField(max_length=36, blank=True, null=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
     deleted_by = models.CharField(max_length=36, blank=True, null=True)
-    is_active = models.BooleanField(blank=True, null=True)
+    is_active = models.BooleanField()
     username = models.TextField(blank=True, null=True)
     email = models.TextField(blank=True, null=True)
     phone_number = models.TextField(blank=True, null=True)
     password = models.TextField()
     last_login = models.DateTimeField(blank=True, null=True)
-    is_verified = models.BooleanField(blank=True, null=True)
+    is_verified = models.BooleanField()
 
     class Meta:
         managed = False
--- api/src/modules/v1/user/queries.py
+++ api/src/modules/v1/user/queries.py
@@ -2,19 +2,43 @@ from django.db.models import Q
 from .models import User
 
 def user_exists(username, email, phone_number):
-    return User.objects.filter(
-        Q(username=username) | Q(email=email) | Q(phone_number=phone_number),
+    user_exist = User.objects.filter(
         is_active=True,
         deleted_at__isnull=True,
     )
 
+    if username:
+        return user_exist.filter(
+            username=username,
+        )
+    if email:
+        return user_exist.filter(
+            email=email,
+        )
+    if phone_number:
+        return user_exist.filter(
+            phone_number=phone_number,
+        )
+
 def user_exists_verified(username, email, phone_number):
-    return User.objects.filter(
-        Q(username=username) | Q(email=email) | Q(phone_number=phone_number),
+    user_exist = User.objects.filter(
         is_active=True,
         deleted_at__isnull=True,
         is_verified=True,
     )
+    
+    if username:
+        return user_exist.filter(
+            username=username,
+        )
+    if email:
+        return user_exist.filter(
+            email=email,
+        )
+    if phone_number:
+        return user_exist.filter(
+            phone_number=phone_number,
+        )
 
 def user_by_id(id):
     return User.objects.filter(
