735ad908ec0b3caeaa019f5e4778352bc9621059
zardarian <zardarianahadika@gmail.com>
2023-03-08 16:38:17 +0700

    remove user from profile serializer

--- api/src/modules/v1/profile/serializers.py
+++ api/src/modules/v1/profile/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from src.modules.v1.dictionary.serializers import DictionarySerializer
-from src.modules.v1.user.serializers import UserSerializer
 from src.modules.v1.pregnancy.serializers import PregnancyRelationSerializer
 from src.modules.v1.interest.serializers import InterestRelationSerializer
 from src.modules.v1.pregnancy.queries import pregnancy_by_profile_code
@@ -9,12 +8,11 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     gender = DictionarySerializer()
-    user = UserSerializer()
     pregnancy = serializers.SerializerMethodField()
     interests = serializers.SerializerMethodField()
     class Meta:
         model = Profile
-        fields = ['id', 'code', 'full_name', 'name_alias', 'birth_date', 'gender', 'avatar', 'slug_name', 'about_me', 'user', 'pregnancy', 'interests']
+        fields = ['id', 'code', 'full_name', 'name_alias', 'birth_date', 'gender', 'avatar', 'slug_name', 'about_me', 'pregnancy', 'interests']
 
     def get_pregnancy(self, obj):
         pregnancy = pregnancy_by_profile_code(obj.code).first()
