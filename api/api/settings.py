643dc3c082e03e68e2caf7fc706d87dab11d0713
zardarian <zardarianahadika@gmail.com>
2023-08-13 22:51:40 +0700

    update celery

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from celery.schedules import crontab
 import os
 import environ
 
@@ -49,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
-    'django_celery_results',
+    'src.queue'
 ]
 
 MIDDLEWARE = [
@@ -194,8 +195,16 @@ GOOGLE_CLIENT_SECRET=env('GOOGLE_CLIENT_SECRET')
 COMPANY_LOGO=env('COMPANY_LOGO')
 COMPANY_HELPER_MAIL=env('COMPANY_HELPER_MAIL')
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_BROKER_URL = os.getenv('CELERY_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_URL')
+CELERY_TIMEZONE = 'UTC'
+CELERY_ENABLE_UTC = True
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_BEAT_SCHEDULE = {
+    "minus": {
+        "task": "src.queue.notification.tasks.minus",
+        "schedule": crontab(minute="*/1"),
+        "args": (16, 15)
+    },
+}
---
+++ api/src/queue/notification/tasks.py
@@ -0,0 +1,47 @@
+from celery import Celery
+from celery.schedules import crontab
+from celery import shared_task
+
+app = Celery()
+
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     print('anjing')
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+#     print('asu')
+#     app.conf.beat_schedule = {
+#         # Executes every Monday morning at 7:30 a.m.
+#         'add-every-minute': {
+#             'task': 'tasks.add',
+#             'schedule': crontab(minute='*/1'),
+#             'args': (16, 16),
+#         },
+#     }
+#     # Calls test('hello') every 30 seconds.
+#     # It uses the same signature of previous task, an explicit name is
+#     # defined to avoid this task replacing the previous one defined.
+#     sender.add_periodic_task(30.0, test.s('hello'), name='add every 30')
+
+#     # Calls test('world') every 30 seconds
+#     sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+#     # Executes every Monday morning at 7:30 a.m.
+#     sender.add_periodic_task(
+#         crontab(hour=7, minute=30, day_of_week=1),
+#         test.s('Happy Mondays!'),
+#     )
+
+@app.task
+def test(arg):
+    print(arg)
+
+@app.task
+def add(x, y):
+    z = x + y
+    print(z)
+
+@shared_task
+def minus(x, y):
+    z = x - y
+    print(z)
