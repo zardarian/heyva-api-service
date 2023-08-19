b13173ad78408f44c3096cc3a6081e362a9fc2a9
zardarian <zardarianahadika@gmail.com>
2023-08-19 19:57:37 +0700

    add end_date

--- api/src/modules/v1/program_personal/queries.py
+++ api/src/modules/v1/program_personal/queries.py
@@ -1,10 +1,12 @@
 from django.db.models import Q
 from .models import ProgramPersonal
+from datetime import datetime
 
 def program_personal_not_finished_by_program_id(profile_code, program_id):
     return ProgramPersonal.objects.filter(
         profile_code=profile_code,
         program=program_id,
         deleted_at__isnull=True,
-        is_finished=False
+        is_finished=False,
+        end_date__gte=datetime.now()
     )
